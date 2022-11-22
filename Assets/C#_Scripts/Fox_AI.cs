0281c5c711fbb7b687cf5854adc81df320199827
SergiuszStein <sergiusz.stein@edu.futuregames.nu>
2022-11-22 14:03:55 +0100

    Fox AI 0.2

--- Assets/C#_Scripts/Fox_AI.cs
+++ Assets/C#_Scripts/Fox_AI.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
+using UnityEngine.Timeline;
 
 public class Fox_AI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("GameObject")]
+    private CharacterController _characterController;
+    
+    [Header("Variables")]
+    [SerializeField] private float _speed;
+    [SerializeField] private float _sniffRadius;
+
+    private void Awake()
     {
-        
+        _characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Physics.OverlapSphere(transform.position, _sniffRadius, LayerMask.NameToLayer("Feathers"));
     }
 }
